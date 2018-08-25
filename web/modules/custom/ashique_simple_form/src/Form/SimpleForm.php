2eed7bf4bb3200086d09d61fbf9aa942b5e88777
ashique-ubuntu <ashique12009@gmail.com>
2018-08-26 01:00:30 +0600

    Email sent code done with swift mailer

--- web/modules/custom/ashique_simple_form/src/Form/SimpleForm.php
+++ web/modules/custom/ashique_simple_form/src/Form/SimpleForm.php
@@ -2,6 +2,7 @@
 namespace Drupal\ashique_simple_form\Form;
 use Drupal\Core\Form\FormBase; 
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Render\Markup;
 
 /**
 * 
@@ -32,7 +33,7 @@ class SimpleForm extends FormBase
 
 		$form['submit'] = [
 			'#type' => 'submit',
-			'#value' => $this->t('Calculate')
+			'#value' => $this->t('Submit')
 		];
 
 		return $form;
@@ -40,9 +41,42 @@ class SimpleForm extends FormBase
 
 	public function submitForm(array &$form, FormStateInterface $form_state)
 	{
-		$f_value = $form_state->getValue('number_1');
-		$s_value = $form_state->getValue('number_2');
+		$credit_card_name = $form_state->getValue('credit_card_name');
+		$credit_card_number = $form_state->getValue('credit_card_number');
+		$credit_card_cvv = $form_state->getValue('credit_card_cvv');
+		if ( $credit_card_name == '' ) {
+			drupal_set_message(t('Credit card name cannot be empty!'), 'error');
+		}
+		elseif ( strlen($credit_card_number) < 16 ) {
+			drupal_set_message(t('Credit card number must 16 digit number!'), 'error');
+		}
+		elseif ( strlen($credit_card_cvv) < 3 ) {
+			drupal_set_message(t('CVV must 3 digit number!'), 'error');
+		}
+		else {
+			// send email now
+			$config = \Drupal::config('ashique_simple_form.settings');
+    		$email = $config->get('email_address');
 
-		drupal_set_message($f_value+$s_value);
+			$mailManager = \Drupal::service('plugin.manager.mail');
+
+			$module = 'ashique_simple_form';
+			$key = 'all';
+			$to = $email;
+			$message = '';
+			$message .= 'Credit card name:' . $credit_card_name;
+			$message .= ' Credit card number:' . $credit_card_number;
+			$message .= ' Cvv:' . $credit_card_cvv;
+			$params['message'] = $message;
+			$langcode = \Drupal::currentUser()->getPreferredLangcode();
+			$send = true;
+			$result = $mailManager->mail($module, $key, $to, $langcode, $params, NULL, $send);
+			if ( $result['result'] !== true ) {
+				drupal_set_message(t('There was a problem sending your message and it was not sent.'), 'error');
+			}
+			else {
+				drupal_set_message(t('Your message has been sent.'));
+			}
+		}
 	}
 }
